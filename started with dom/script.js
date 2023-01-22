d0d83a6810fde1cf29df0cc05cf37efb40449697
saurabh <mahtosaurabh1@gmail.com>
2023-01-22 22:27:57 +0530

    one more list is added

--- started with dom/script.js
+++ started with dom/script.js
@@ -1,5 +1,6 @@
 let title=document.querySelector('#main-header');
 let item=document.querySelectorAll('.list-group-item')
+let ulist=document.querySelector('#items')
 
 // title.style.border='2px solid black';
 
@@ -8,8 +9,13 @@ let item=document.querySelectorAll('.list-group-item')
 //     item[i].style.fontSize='3rem'
 // } 
 
-item[2].style.backgroundColor='green';
+// item[2].style.backgroundColor='green';
 
-for(let i=0;i<item.length;i++){
-    item[i].style.fontWeight='bold'
-} 
+// for(let i=0;i<item.length;i++){
+//     item[i].style.fontWeight='bold'
+// } 
+
+let li=document.createElement('li');
+li.classList.add('list-group-item');
+li.innerText='Item 5';
+ulist.appendChild(li)
