ada79275eb5663836de0457585a45be18356630a
saurabh <mahtosaurabh1@gmail.com>
2023-01-22 22:14:22 +0530

    make fontweight bold and change background color of third element is green

--- started with dom/script.js
+++ started with dom/script.js
@@ -1,10 +1,15 @@
 let title=document.querySelector('#main-header');
 let item=document.querySelectorAll('.list-group-item')
 
-title.style.border='2px solid black';
+// title.style.border='2px solid black';
+
+// for(let i=0;i<item.length;i++){
+//     item[i].style.color='green'
+//     item[i].style.fontSize='3rem'
+// } 
+
+item[2].style.backgroundColor='green';
 
 for(let i=0;i<item.length;i++){
-    item[i].style.color='green'
-    item[i].style.fontSize='3rem'
+    item[i].style.fontWeight='bold'
 } 
-
