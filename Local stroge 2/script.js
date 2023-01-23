68b6450eb47a29fab35e6f75eb9b2e2eaa9d21ef
saurabh <mahtosaurabh1@gmail.com>
2023-01-23 19:19:05 +0530

    mobile no. added to local storage in app

--- Local stroge 2/script.js
+++ Local stroge 2/script.js
@@ -2,6 +2,7 @@ let name=document.querySelector('.name')
 let email=document.querySelector('.email')
 let container=document.querySelector('.container')
 let btn=document.querySelector('.btn')
+let mbno=document.querySelector('.mbno')
 
 let ls=localStorage.getItem('tickets');
 let db=ls?JSON.parse(ls):[];
@@ -19,20 +20,22 @@ btn.addEventListener('click',()=>{
     n.trim();
     let e=email.value;
     e.trim();
-   if(n === '' || e === ''){
-    alert('please enter name and email')
+    let mn=mbno.value;
+   if(n === '' || e === '' || mn==''){
+    alert('please enter name and email and mobile number')
    }else{
-        db.push({n,e});
-        createTicket(n,e);
+        db.push({n,e,mn});
+        createTicket(n,e,mn);
    }
 })
 
-function createTicket(n,e){
+function createTicket(n,e,mn){
     let divtag=document.createElement('div');
-    divtag.innerHTML=`<div class="child">${n} : ${e} </div>`
+    divtag.innerHTML=`<div class="child">Name ->${n} : Email -> ${e} : Mb No.  ${mn} </div>`
     container.appendChild(divtag);
     name.value='';
     email.value='';
+    mbno.value='';
     localStorage.setItem('tickets',JSON.stringify(db));
 }
 
