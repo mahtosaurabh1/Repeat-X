6b82d7d15ea25b7e4aeabcaf5de907255eacbce0
saurabh <mahtosaurabh1@gmail.com>
2023-01-24 03:49:38 +0530

    added delete button and functionality to delete from local storage

--- Local stroge 2/script.js
+++ Local stroge 2/script.js
@@ -1,8 +1,10 @@
-let name=document.querySelector('.name')
+let Uname=document.querySelector('.name')
 let email=document.querySelector('.email')
 let container=document.querySelector('.container')
 let btn=document.querySelector('.btn')
-let mbno=document.querySelector('.mbno')
+let mbno=document.querySelector('.mbno');
+
+
 
 let ls=localStorage.getItem('tickets');
 let db=ls?JSON.parse(ls):[];
@@ -10,13 +12,13 @@ let db=ls?JSON.parse(ls):[];
 
 if(db){
     db.map((val)=>{
-    createTicket(val.n,val.e,val.mn)
+    createTicket(val.n,val.e,val.mn,val.uid)
       })
 }
 
 
 btn.addEventListener('click',()=>{
-    let n=name.value;
+    let n=Uname.value;
     n.trim();
     let e=email.value;
     e.trim();
@@ -24,18 +26,44 @@ btn.addEventListener('click',()=>{
    if(n === '' || e === '' || mn==''){
     alert('please enter name and email and mobile number')
    }else{
-        db.push({n,e,mn});
-        createTicket(n,e,mn);
+        let uid=Math.random();
+        db.push({n,e,mn,uid});
+        createTicket(n,e,mn,uid);
+        // console.log(uid)
    }
 })
 
-function createTicket(n,e,mn){
+function createTicket(n,e,mn,ticketId){
+    // console.log(uid)
     let divtag=document.createElement('div');
-    divtag.innerHTML=`<div class="child">Name ->${n} : Email -> ${e} : Mb No.  ${mn} </div>`
+    divtag.classList.add('parentcont');
+    divtag.innerHTML=`<div class="child">Name ->${n} : Email -> ${e} : Mb No.  ${mn}</div>
+                      <button class='delete' onclick='handleRemove(${ticketId})'>Delete</button>`
+
+    
     container.appendChild(divtag);
-    name.value='';
+
+    let del=divtag.querySelector('.delete');
+    del.addEventListener('click',()=>{
+        divtag.remove();
+    })
+
+    Uname.value='';
     email.value='';
     mbno.value='';
     localStorage.setItem('tickets',JSON.stringify(db));
+
+}
+
+function handleRemove(uniqueId){
+      let idx=-1;
+      for(let i=0;i<db.length;i++){
+        if(db[i].uid === uniqueId){
+            idx=i;
+            break;
+        }
+       }
+       db.splice(idx,1);
+       localStorage.setItem('tickets',JSON.stringify(db));
 }
 
