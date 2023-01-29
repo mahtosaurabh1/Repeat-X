426738d400dce1c15bd4d36ba797398a3ed71361
saurabh <mahtosaurabh1@gmail.com>
2023-01-29 13:53:54 +0530

    dynamic registration page completed

--- appointment using crud web/script.js
+++ appointment using crud web/script.js
@@ -4,10 +4,12 @@ let container=document.querySelector('.container')
 let btn=document.querySelector('.btn')
 let mbno=document.querySelector('.mbno');
 
-
+// first link -  https://crudcrud.com/api/c48ffc4d81ac4d6bb60749a9b90a986b/appointment
+// second link -  https://crudcrud.com/api/16b47d3e52d949db87ce6df743df8b9d/appointment
+// third link - https://crudcrud.com/api/ba044c505c4841d5ba17458008c5ca10
 
 async function  fetchOldData(){
-    let d= await axios.get('https://crudcrud.com/api/c48ffc4d81ac4d6bb60749a9b90a986b/appointment');
+    let d= await axios.get('https://crudcrud.com/api/ba044c505c4841d5ba17458008c5ca10/appointment');
      d.data.map((val)=>{
         // console.log(val._id);
         createTicket(val.n,val.e,val.mn,val._id);
@@ -24,29 +26,35 @@ btn.addEventListener('click',()=>{
    if(n === '' || e === '' || mn==''){
     alert('please enter name and email and mobile number')
    }else{
-       let obj={n,e,mn};
-        axios.post('https://crudcrud.com/api/c48ffc4d81ac4d6bb60749a9b90a986b/appointment',obj)
-        .then((resolve)=>{
-            createTicket(resolve.data.n,resolve.data.e,resolve.data.mn)
-            console.log(resolve.data)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+    let obj={n,e,mn};
+    axios.post('https://crudcrud.com/api/ba044c505c4841d5ba17458008c5ca10/appointment',obj)
+    .then((resolve)=>{
+        createTicket(resolve.data.n,resolve.data.e,resolve.data.mn,resolve.data._id);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    
    }
 })
 
-async function createTicket(n,e,mn,id){
-    // console.log(uid)
+
+
+
+
+function createTicket(n,e,mn,userId){
+
+    // console.log("id "+userId)
     let divtag=document.createElement('div');
     divtag.classList.add('parentcont');
     divtag.innerHTML=`<div class="child cName">${n}</div>
                         <div class="child cEmail ">${e} </div>
                         <div class="child cPhno" > ${mn}</div>
-                         <button class='delete' onclick='handleRemove(${id})'>Delete</button>
+                         <button class='delete' >Delete</button>
                          <div class='update'  ><i class="fa fa-lock"></i></div>`
 
     
+
     container.appendChild(divtag);
     Uname.value='';
     email.value='';
@@ -70,8 +78,9 @@ async function createTicket(n,e,mn,id){
             cName.setAttribute('contenteditable','false');
             cEmail.setAttribute('contenteditable','false');
             cPhno.setAttribute('contenteditable','false');
+
             let updObj={"n":cName.textContent,"e":cEmail.textContent,"pn":cPhno.textContent}
-            axios.put(`https://crudcrud.com/api/c48ffc4d81ac4d6bb60749a9b90a986b/appointment/${id}`,updObj)
+            axios.put(`https://crudcrud.com/api/ba044c505c4841d5ba17458008c5ca10/appointment/${userId}`,updObj)
         }
     })
 
@@ -79,13 +88,20 @@ async function createTicket(n,e,mn,id){
     let del=divtag.querySelector('.delete');
     del.addEventListener('click',()=>{
         divtag.remove();
+
+        axios.delete(`https://crudcrud.com/api/ba044c505c4841d5ba17458008c5ca10/appointment/${userId}`)
+        .then((res)=>{
+            console.log(res)
+        }).catch((er)=>{
+            console.log(er);
+        })
     })
+   
 
 }
 
-async function handleRemove(id){
-    let a=await  axios.delete(`https://crudcrud.com/api/c48ffc4d81ac4d6bb60749a9b90a986b/appointment/${id}`)
-    console.log(a)
-}
+
+
+
 
 
