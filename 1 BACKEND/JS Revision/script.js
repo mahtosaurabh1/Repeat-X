ae8d16a21b6bdb815ec24617ff7fa39fc48ec2df
saurabh <mahtosaurabh1@gmail.com>
2023-02-08 01:30:41 +0530

    arrow function and object

---
+++ 1 BACKEND/JS Revision/script.js
@@ -0,0 +1,20 @@
+// arraow function
+
+// let val=(a,b)=>{
+//     return a*b;
+// }
+
+// console.log(val(2,3));
+
+// object
+
+let o={
+    Name:"aman",
+    Age:20,
+     greet:()=>{
+        console.log(this.Name)
+    }
+}
+
+o.greet();
+
