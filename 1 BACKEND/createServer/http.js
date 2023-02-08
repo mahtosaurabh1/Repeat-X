03f9541d8bf5ce3ffcfb1d3a64138cde3bc4a6f5
saurabh <mahtosaurabh1@gmail.com>
2023-02-09 02:45:33 +0530

    read and write in file

--- 1 BACKEND/createServer/http.js
+++ 1 BACKEND/createServer/http.js
@@ -1,15 +1,45 @@
 let http = require("http");
+let fs =require('fs');
 
 const app = http.createServer(function(req,res){
-    // res.write("Hello world");
     let url=req.url;
-    if(url === '/home'){
-        res.write("welcome to home")
-    }else if(url==='/about'){
-        res.write('welcome to about us page')
-    }else if(url === '/node'){
-        res.write('welcome to our nodejs project')
-    }
+    let method=req.method;
+    // if(url === '/home'){
+    //     res.write("welcome to home")
+    // }else if(url==='/about'){
+    //     res.write('welcome to about us page')
+    // }else if(url === '/node'){
+    //     res.write('welcome to our nodejs project')
+    // }
+
+   if(url === '/'){
+    res.write('<html><head></head><body> <form method="POST" action="/message"> <input name="message type="text"> <button>submit</button type="submit"> </form></body></html>');
+
+    let d=fs.readFileSync('./message.txt');
+     res.write(d);
+    return res.end();
+   }
+
+   if(url === '/message'  && method === "POST"){
+    const body=[];
+    req.on('data',(chunk)=>{
+        // console.log("chunk",chunk)
+        body.push(chunk);
+    })
+    req.on('end',()=>{
+        let parsedBody=Buffer.concat(body).toString();
+        let message=parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt',message);
+        console.log("message ",message);
+    })
+
+    
+
+    res.statusCode=302;
+    res.setHeader('Location','/');
+    return  res.end();
+   }
+    
     res.end();
 })
 
