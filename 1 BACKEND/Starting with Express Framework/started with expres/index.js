74e68a8e6ae12a7fb8f46fb4f89058d1ba4b5cef
saurabh <mahtosaurabh1@gmail.com>
2023-02-11 03:25:25 +0530

    itro to middleware

--- 1 BACKEND/Starting with Express Framework/started with expres/index.js
+++ 1 BACKEND/Starting with Express Framework/started with expres/index.js
@@ -1,13 +1,25 @@
 let express=require('express');
-
+let bodyParser=require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("first middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+// app.use('/',(req,res,next)=>{
+//     console.log("this always run");
+//     next();
+// })
+
+app.use('/add-product',(req,res,next)=>{
+    console.log("add product");
+    res.send('<form method="POST" action="/product"><input name="product" type="text" /> <input name="product" type="text" /> <button type="submit">submit</button></form>')
 })
 
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
     console.log("second middleware");
     res.send('<h1>hello</h1>')
 })
