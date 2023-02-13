6290fa55d85943d18ebc5cae5b202298b3f8724d
saurabh <mahtosaurabh1@gmail.com>
2023-02-13 20:55:52 +0530

    props done

--- 2 REACT/expance-tracker/src/App.js
+++ 2 REACT/expance-tracker/src/App.js
@@ -3,9 +3,28 @@ import './App.css';
 import ExpanceItem from './componets/ExpanceItem';
 
 function App() {
+  const expances=[{
+    id:1,
+    title:"movie",
+    location:"delhi",
+    price:200,
+    date:new Date(2023,1,1)
+  },{
+    id:2,
+    title:"play",
+    location:"delhi",
+    price:200,
+    date:new Date(2023,1,1)
+  },{
+    id:3,
+    title:"book",
+    location:"delhi",
+    price:200,
+    date:new Date(2023,1,1)
+  },]
   return (
     <>
-    <ExpanceItem/>
+    <ExpanceItem props={expances}/>
     </>
   );
 }
--- 2 REACT/expance-tracker/src/componets/ExpanceItem.js
+++ 2 REACT/expance-tracker/src/componets/ExpanceItem.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import './expanceitem.css'
-function ExpanceItem() {
+function ExpanceItem({props}) {
     const date=new Date(2023,1,1);
     let item='Food';
     let price=500;
     let location='delhi'
+    console.log(props);
   return (
-    <div className='expense-item'>
-    <h1>{date.toISOString()}</h1>
-    <div className='expense-item__description'>
-       <h2>{item}</h2>
-       <h2>{location}</h2>
-       <div className='expense-item__price'>{price}</div>
-    </div>
-    </div>
-  )
+    <>
+    {
+        props.map((val)=>{
+            return (
+                <div className='expense-item' key={val.id}>
+                <h1>{val.date.toString()}</h1>
+                <div className='expense-item__description'>
+                   <h2>{val.title}</h2>
+                   <h2>{val.location}</h2>
+                   <div className='expense-item__price'>{val.price}</div>
+                </div>
+                </div>
+            )
+        })
+    }
+    </>
+    )
 }
 
 export default ExpanceItem
