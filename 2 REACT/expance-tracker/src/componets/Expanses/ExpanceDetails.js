0a9a7e7e90de7c2a031e8925810ae8127f84d758
saurabh <mahtosaurabh1@gmail.com>
2023-02-16 14:34:57 +0530

    update price function added

--- 2 REACT/expance-tracker/src/componets/Expanses/ExpanceDetails.js
+++ 2 REACT/expance-tracker/src/componets/Expanses/ExpanceDetails.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 let ExpanceDetails=(props)=>{
+  const [price,setPrice]=useState(props.props.price);
+  let updatePrice=(id)=>{
+    setPrice(100);
+   }
   return (
     <>
             <div className='expense-item__description'>
                    <h2>{props.props.title}</h2>
                    <h2>{props.props.location}</h2>
-                   <div className='expense-item__price'>{props.props.price}</div>
+                   <div className='expense-item__price'>${price}</div>
+                   <button onClick={()=>updatePrice(props.props.id)}>update price</button>
                 </div>
     </>
   )
--- 2 REACT/expance-tracker/src/componets/Expanses/ExpanceItem.js
+++ 2 REACT/expance-tracker/src/componets/Expanses/ExpanceItem.js
@@ -6,29 +6,26 @@ import './expanceitem.css'
 let  ExpanceItem= ({props})=>{
 
     let [arr,setArr]=useState(props);
+
     let handleDelete=(id)=>{
         let nArr=props.filter((val)=>{
             return val.id != id;
         })
         setArr(nArr);
     }
+
+   
   return (
     <>
     {
         arr.map((val)=>{
             return (
-                // <Card className='expense-item' key={val.id}>
                 <div className='expense-item' key={val.id}>
                     <Card/>
-                {/* <h1>{val.date.toISOString()}</h1> */}
                 <ExpanceDate props={val}/>
-                {/* <div className='expense-item__description'>
-                   <h2>{val.title}</h2>
-                   <h2>{val.location}</h2>
-                   <div className='expense-item__price'>{val.price}</div>
-                </div> */}
                 <ExpanceDetails props={val}/>
                 <button onClick={()=>handleDelete(val.id)}>Delete Expanse</button>
+                
                 </div>
                 // </Card>
             )
