b7dda9b7ebc5119db084fe7331b5bf259377d0e3
saurabh <mahtosaurabh1@gmail.com>
2023-02-18 01:25:16 +0530

    all expances rendere

--- 2 REACT/expance-tracker/src/App.js
+++ 2 REACT/expance-tracker/src/App.js
@@ -1,42 +1,52 @@
-import { useState } from 'react';
-import './App.css';
-import ExpanceItem from './componets/Expanses/ExpanceItem';
-import NewEpanses from './componets/NewExpanses/newEpanses';
+import React, { useState } from 'react';
 
-let  App=()=> {
- let [arr,setArr]=useState([]);
-  // const expances=[{
-  //   id:1,
-  //   title:"movie",
-  //   location:"Samastipur",
-  //   price:200,
-  //   date:new Date("2023-03-25")
-  // },{
-  //   id:2,
-  //   title:"play",
-  //   location:"delhi",
-  //   price:200,
-  //   date:new Date("2022-03-25")
-  // },{
-  //   id:3,
-  //   title:"book",
-  //   location:"Mumbai",
-  //   price:200,
-  //   date:new Date("2024-03-25")
-  // },]
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
-  const addExpanseHandler=(expanse)=>{
-    arr.push(expanse);
-    // setArr(...arr,expanse);
-    // console.log(arr);
-    <ExpanceItem props={arr}/>
-  }
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-    <NewEpanses onAddExpanses={addExpanseHandler} />
-    <ExpanceItem props={arr}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- 2 REACT/expance-tracker/src/componets/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-let  Card=(props)=>{
-  return (
-    <div className='card'>{props.children}</div>
-  )
-}
-
-export default Card
