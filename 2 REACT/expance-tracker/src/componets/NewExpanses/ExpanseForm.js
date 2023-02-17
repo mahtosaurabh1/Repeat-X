5b44c71d655ba16945a67230f362cf9072429ec1
saurabh <mahtosaurabh1@gmail.com>
2023-02-17 19:33:35 +0530

    useState added in form component

--- 2 REACT/expance-tracker/src/componets/NewExpanses/ExpanseForm.js
+++ 2 REACT/expance-tracker/src/componets/NewExpanses/ExpanseForm.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExapnseForm.css'
 function ExpanseForm() {
-    let handleInput = (e)=>{
-        console.log(e.target.value);
+    const [title,setTitle]=useState();
+    const [amount,setAmount]=useState();
+    const [date,setDate]=useState();
+    let handleTitleInput = (e)=>{
+        setTitle(e.target.value);
+    }
+
+    let handleAmountInput=(e)=>{
+        setAmount(e.target.value);
+    }
+
+    let handleDateInput=(e)=>{
+        setDate(e.target.value);
     }
   return (
     <form action="">
         <div className="new-expense__control">
             <div className='new-expense__control'>
                 <label >Title</label>
-                <input type="text" onChange={handleInput} />
+                <input type="text" onChange={handleTitleInput} />
             </div>
             <div className='new-expense__control'>
                 <label >Amount</label>
-                <input type="number" onChange={handleInput} />
+                <input type="number" onChange={handleAmountInput} />
             </div>
             <div className='new-expense__control'>
                 <label >Date</label>
-                <input type="date" onChange={handleInput} />
+                <input type="date" onChange={handleDateInput} />
             </div>
         </div>
         <div className='new-expense__actions'>
