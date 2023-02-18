14ce6e102d9241dc8cdb70bd9c7a6422da659198
saurabh <mahtosaurabh1@gmail.com>
2023-02-18 23:56:54 +0530

    useReducer implemented

--- 2 REACT/3 login dummy page/login udemy/src/App.js
+++ 2 REACT/3 login dummy page/login udemy/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,21 +7,23 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
- useEffect(()=>{
-  let data=localStorage.getItem('logedIn');
-  if(data==='1'){
-   setIsLoggedIn(true);
-  }
- },[])
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('logedIn','1');
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
