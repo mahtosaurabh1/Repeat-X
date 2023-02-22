0cf02f21fce7d83066e16f270f802e4cc7f455a4
saurabh <mahtosaurabh1@gmail.com>
2023-02-23 00:14:15 +0530

    cart open close done

--- 2 REACT/RestaruantApp/restrauntapp/src/App.js
+++ 2 REACT/RestaruantApp/restrauntapp/src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown,setCartIsShown]=useState(false);
+
+  let showCartHandler=()=>{
+    setCartIsShown(true);
+  }
+
+  let hideCartHandler=()=>{
+    setCartIsShown(false);
+  }
   return (
     <Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals/>
       </main>
