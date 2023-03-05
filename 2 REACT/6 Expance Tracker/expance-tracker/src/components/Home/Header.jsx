cf7189cdbd68dc5c4422b52dd08664f703bc022c
saurabh <mahtosaurabh1@gmail.com>
2023-03-05 10:30:24 +0530

    logout functionality added

--- 2 REACT/6 Expance Tracker/expance-tracker/src/components/Home/Header.jsx
+++ 2 REACT/6 Expance Tracker/expance-tracker/src/components/Home/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import AuthContext from '../../store/auth-context';
 import './header.css'
 function Header() {
+  let authCtx=useContext(AuthContext);
+  let navigate=useNavigate();
+  let logoutHandler=()=>{
+    authCtx.logout();
+    navigate('/login')
+  }
   return (
     <div className="header-container">
       <h3>Welcome to Expanse tracker</h3>
       <p>Your Profile is Incomple <Link to='/profile'><button>Complete Now</button></Link></p>
+      <Link to='/login'>Login</Link>
+      <Link to='/singup'>Create Account</Link>
+      <button onClick={logoutHandler}>Logout</button>
     </div>
   )
 }
