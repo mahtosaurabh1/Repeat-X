abe476383691088232ba8a5e08e029042093c113
saurabh <mahtosaurabh1@gmail.com>
2023-03-08 01:24:49 +0530

    login page completed

--- 2 REACT/9 Mail-Box-Client/mail-box-client/mail-box/src/App.js
+++ 2 REACT/9 Mail-Box-Client/mail-box-client/mail-box/src/App.js
@@ -1,11 +1,17 @@
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 
 function App() {
   return (
   <Router>
-    <Signup/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/singup' element={ <Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
   </Router>
   );
 }
