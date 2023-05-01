ab1ea219679152fdc822dacb13fc8fdfb2d3e4c4
saurabh <mahtosaurabh1@gmail.com>
2023-05-01 19:35:37 +0530

    mail send api created

--- 1 BACKEND/1_Backend_Phase-2/6 expense-tracker/backend/index.js
+++ 1 BACKEND/1_Backend_Phase-2/6 expense-tracker/backend/index.js
@@ -3,6 +3,9 @@ const cors=require('cors');
 const bodyParser = require('body-parser');
 const User = require('./Model/User');
 const Expenses = require('./Model/Expenses');
+const otpSchema=require('./Model/otp')
+var nodemailer = require('nodemailer');
+
 require('./db/config')
 
 
@@ -57,6 +60,59 @@ app.put('/expenses/:id',async (req,res)=>{
     let result=await Expenses.updateOne({_id:req.params.id},{$set:req.body})
     res.send(result)
 })
+
+
+
+// ___________________forget password
+app.post('/email-send',async (req,res)=>{
+    let data=await User.findOne({email:req.body.email});
+    if(data){
+        let otpCode=Math.floor((Math.random()*10000)+1);
+        let otpData =new otpSchema({
+            email:req.body.email,
+            code:otpCode
+        })
+        let result=await otpData.save();
+        mailsend('2356','mahtosaurabh1@gmail.com');
+        res.send(result)
+    }
+})
+
+app.post('/change-password',async (req,res)=>{
+    let data=await otpSchema.find({email:req.body.email,code:req.body.code});
+    if(data){
+        let user=await User.findOne({email:req.body.email});
+        user.password=req.body.password;
+        user.save();
+    }
+
+})
+
+const  mailsend=(mail,otp)=>{
+    
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mahtosaurabh1@gmail.com',
+      pass: '123'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'mahtosaurabh1@gmail.com',
+    to: 'mahtosaurabh77@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+ }
 
 
 
