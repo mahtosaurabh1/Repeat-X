7aac99aa434468d7ecb603e030172b2cd8e0a72b
saurabh <mahtosaurabh1@gmail.com>
2023-06-03 14:35:31 +0530

    Frontend Added

--- 1 BACKEND/1_Backend_Phase-2/7 ChatApp/frontend/src/App.js
+++ 1 BACKEND/1_Backend_Phase-2/7 ChatApp/frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter as Router ,Route ,Routes} from 'react-router-dom'
 import Signup from './components/Signup/Signup.jsx'
 import Login from './components/Login/Login';
+import ChatHome from './components/chatAppHome/ChatHome';
 
 function App() {
   return (
    <Router>
     <Routes>
+      <Route path='/' element={<ChatHome/>}/>
       <Route path='/register' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
---
+++ 1 BACKEND/1_Backend_Phase-2/7 ChatApp/frontend/src/components/chatAppHome/ChatHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ChatHome() {
+  return (
+    <div><ul>
+        <li>Aman Join</li>
+        <li>Vaibhav Join</li>
+        <li>Hello Vaibhav</li>
+        <li>hi</li>
+        </ul></div>
+  )
+}
+
+export default ChatHome
